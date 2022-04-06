3cb592877d52bda61fc711e7db7ff4b741816e9f
Ahad Lag <mr.ahad.lag@gmail.com>
2022-04-06 16:19:21 +0430

    add removeItem method for delete item to localStorage and dataTable

--- js/main.js
+++ js/main.js
@@ -30,51 +30,55 @@
          if(items !== null){
              let table_body = document.createElement('tbody');
              table_body.id = 'table-body';
+             let counter = 0;
              for(let i=0;i<items.length;i++) {
-                 // for total income and cost
-                 if(items[i].type == 'هزینه'){
-                     cost += Number(items[i].price);
-                 }else if(items[i].type == 'درآمد'){
-                     income += Number(items[i].price);
-                 }
-                 // for data entry table from localStorage
-                 let tr_subject = document.createElement('tr');
-                 table_body.appendChild(tr_subject);
-                 let td_row = document.createElement('td');
-                 td_row.textContent = this.numberToPersian(i + 1);
-                 tr_subject.appendChild(td_row);
-                 let td_price = document.createElement('td');
-                 td_price.textContent = this.numberToPersian(this.separate(items[i].price));
-                 tr_subject.appendChild(td_price);
-                 let td_date = document.createElement('td');
-                 let date = items[i]['date'].year + '/' + items[i]['date'].month + '/' + items[i]['date'].day;
-                 td_date.textContent = this.numberToPersian(date);
-                 tr_subject.appendChild(td_date);
-                 let td_type = document.createElement('td');
-                 td_type.textContent = items[i].type;
-                 if(items[i].type == 'هزینه') {
-                     td_type.setAttribute('class', 'text-danger');
-                 }else if(items[i].type == 'درآمد'){
-                     td_type.setAttribute('class', 'text-success');
-                 }
-                 tr_subject.appendChild(td_type);
-                 let td_desc = document.createElement('td');
-                 let a_desc = document.createElement('a');
-                 a_desc.textContent = 'نمایش';
-                 a_desc.setAttribute('class', 'btn btn-primary btn-sm me-2 px-2');
-                 a_desc.setAttribute('data-bs-toggle', 'modal');
-                 a_desc.setAttribute('data-bs-target', '#descModal');
-                 a_desc.setAttribute('onclick', 'document.getElementById("desc_text").textContent = "'+ items[i].desc +'";');
-                 let a_delete = document.createElement('a');
-                 a_delete.textContent = 'حذف';
-                 a_delete.setAttribute('class', 'btn btn-outline-danger btn-sm px-2');
-                 a_delete.setAttribute('data-bs-toggle', 'modal');
-                 a_delete.setAttribute('data-bs-target', '#deleteModal');
-                 td_desc.appendChild(a_desc);
-                 td_desc.appendChild(a_delete);
+                 if(items[i].is_deleted == false){
+                     // for total income and cost
+                     if(items[i].type == 'هزینه'){
+                         cost += Number(items[i].price);
+                     }else if(items[i].type == 'درآمد'){
+                         income += Number(items[i].price);
+                     }
+                     // for data entry table from localStorage
+                     let tr_subject = document.createElement('tr');
+                     table_body.appendChild(tr_subject);
+                     let td_row = document.createElement('td');
+                     td_row.textContent = this.numberToPersian(counter += 1);
+                     tr_subject.appendChild(td_row);
+                     let td_price = document.createElement('td');
+                     td_price.textContent = this.numberToPersian(this.separate(items[i].price));
+                     tr_subject.appendChild(td_price);
+                     let td_date = document.createElement('td');
+                     let date = items[i]['date'].year + '/' + items[i]['date'].month + '/' + items[i]['date'].day;
+                     td_date.textContent = this.numberToPersian(date);
+                     tr_subject.appendChild(td_date);
+                     let td_type = document.createElement('td');
+                     td_type.textContent = items[i].type;
+                     if(items[i].type == 'هزینه') {
+                         td_type.setAttribute('class', 'text-danger');
+                     }else if(items[i].type == 'درآمد'){
+                         td_type.setAttribute('class', 'text-success');
+                     }
+                     tr_subject.appendChild(td_type);
+                     let td_desc = document.createElement('td');
+                     let a_desc = document.createElement('a');
+                     a_desc.textContent = 'نمایش';
+                     a_desc.setAttribute('class', 'btn btn-primary btn-sm me-2 px-2');
+                     a_desc.setAttribute('data-bs-toggle', 'modal');
+                     a_desc.setAttribute('data-bs-target', '#descModal');
+                     a_desc.setAttribute('onclick', 'document.getElementById("desc_text").textContent = "'+ items[i].desc +'";');
+                     let a_delete = document.createElement('a');
+                     a_delete.textContent = 'حذف';
+                     a_delete.setAttribute('class', 'btn btn-outline-danger btn-sm px-2');
+                     a_delete.setAttribute('data-bs-toggle', 'modal');
+                     a_delete.setAttribute('data-bs-target', '#deleteModal');
+                     a_delete.setAttribute('onclick', 'document.getElementById("btn_delete").setAttribute("onclick","main.removeItem('+ items[i].id +')");document.getElementById("btn_delete").setAttribute("data-bs-dismiss", "modal");');
+                     td_desc.appendChild(a_desc);
+                     td_desc.appendChild(a_delete);
 
-                 tr_subject.appendChild(td_desc);
-                 document.getElementById('dataTable').appendChild(table_body);
+                     tr_subject.appendChild(td_desc);
+                     document.getElementById('dataTable').appendChild(table_body);
+                 }
              }
              // for data entry in total box
              document.getElementById('total-cost').textContent = this.numberToPersian(this.separate(cost));
@@ -115,6 +119,16 @@
              ];
              localStorage.setItem('financialManagement',JSON.stringify(newItem));
          }
+         this.loadPage();
+     }
+     removeItem(id){
+         let items = JSON.parse(localStorage.getItem('financialManagement'));
+         for(let i=0;i<items.length;i++){
+             if(items[i].id == id){
+                 items[i].is_deleted = true;
+             }
+         }
+         localStorage.setItem('financialManagement',JSON.stringify(items));
          this.loadPage();
      }
  }
